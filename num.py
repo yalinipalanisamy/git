2b25dd618b19c763ec70b2a8e1147dc7da6d5109
yalinipalanisamy <51122401+yalinipalanisamy@users.noreply.github.com>
2019-05-29 10:37:33 +0530

    Update num.py

--- num.py
+++ num.py
@@ -1,9 +1,10 @@
-  a=int(input("enter the value"))
-print("the value is negative")
-elif(a==0):
+a=int(input("enter the value"))
 if(a>=0):
   print("the value is positive")
 elif(a<=0):
+  print("the value is negative")
+elif(a==0):
   print("the value is zero")
 else:
   print("invalid value")
+
